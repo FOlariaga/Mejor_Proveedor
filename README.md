e2c2cb3c6ebb824abf53b69735e13eab1a0e12dc
FOlariaga <olariagafederico2@gmail.com>
2023-09-13 19:56:30 -0300

    archivo readme, funcion en el script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mejor Proveedor
-busca comparar distintas listas de proveederos para poder recomendar la opcion mas economica basandose en su proveedores para comprar su producto
+el proposito de este proyecto es comparar el precio que tiene un producto entre distintos proveedores, en un principio no hay limites para la cantidad de proveedores pero la idea es ir mejorando el proyecto para poder aumentar la cantidad de productos que se puede comparar y no que se haga de uno en uno.
+
+En lineas generales se determina la cantidad de proveedores para poder repetir la cantidad de veces que el bucle debe preguntar el precio.
+
+El primer precio se determinara autometicamente como la mejor opcion de compra ya que es la unica hasta el momento, luego de ingresar el segundo precio se compara con el mejor precio del momento para determinar si es mayor o menos que el, en el caso de ser menor se pregunta el nombre de ese proveedor para poder asignarlo como el nuevo mejor precio y en caso contrario se descartara directamente pasando a preguntar un precio nuevo hasta llegar al limite de proveedores.
+
+luego de ello se ejecuta un alert para informar cual es el proveedor con el producto al precio mas barato para posteriormente preguntar si se quiere repetir el proceso con otro producto.
--- script/main.js
+++ script/main.js
@@ -1,12 +1,23 @@
+// declaracion de la funcion que dara el resultado
+function conclusion(proveedor, producto, precio) {
+    return "El proveedor " + proveedor + " tiene el/la " + producto + " mas barato a un precio de $" + precio + ", en comparacion con los demas.";
+}
+
+
 const CANTIDAD_DE_PROVEEODORES = parseInt(prompt("¿Cuantos proveedores desea comparar?"));
 let otroProducto = false;
+console.log(CANTIDAD_DE_PROVEEODORES)
 do {
+    if (CANTIDAD_DE_PROVEEODORES.toString() == "NaN" || CANTIDAD_DE_PROVEEODORES < 1) {
+        alert("Debe ingresar un numero valido mayor a 0")
+        break
+    }
     const PRODUCTO = prompt("¿Cual es el nombre del producto a comparar?");
     let mejorPrecio = Infinity;
     let mejorProveedor = "";
 
     for (let i = 1; i <= CANTIDAD_DE_PROVEEODORES; i++) {
-        let precio = parseInt(prompt("¿Cual es el precio de el/la " + PRODUCTO + " determinado por su proveedor N° " + i + "?"));
+        let precio = parseFloat(prompt("¿Cual es el precio de el/la " + PRODUCTO + " determinado por su proveedor N° " + i + "?"));
 
         if (mejorPrecio > precio) {
             mejorPrecio = precio;
@@ -18,7 +29,8 @@ do {
         }
     }
 
-    alert("El proveedor " + mejorProveedor + " tiene el/la " + PRODUCTO + " mas barato a un precio de $" + mejorPrecio + ", en comparacion con los demas.");
+    //     llamado a la funcion
+    alert(conclusion(mejorProveedor,PRODUCTO,mejorPrecio));
 
     mejorPrecio = Infinity;
     mejorProveedor = "";
